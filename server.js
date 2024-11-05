db3647088e22ee4d6c7a7a4589369624588c0afb
ganesh-digi <msganesh@digisprint.com>
2024-11-05 14:09:51 +0530

    razorpay backend code push

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql2/promise');
-const { OAuth2Client } = require('google-auth-library');
-const axios = require('axios');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const mysql = require("mysql2/promise");
+const { OAuth2Client } = require("google-auth-library");
+const axios = require("axios");
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Razorpay = require("razorpay");
 const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 
-RAZORPAY_SECRET="mRcMlDUqNU21VNSiVUi9pxpg"
-RAZORPAY_KEY_ID="rzp_live_L6Fy6yBDycyCzw"
-EMAIL_SECRET_KEY="3oT8F4sm02jUIxoT91@ApxvPQ1z!0@"
-GOOGLE_CLIENT_ID="378678297066-q6qeqtpfh0ih4e99lv887o1rgduehs9u.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET="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"
-GITHUB_CLIENT_ID = "Ov23livsiN32CRBf7KtH";
-GITHUB_CLIENT_SECRET = "c206aa7f4048f95e01d49ce4b58c05ba6617a724";
-DATABASE_TYPE="mysql"
-DATABASE_PORT="3306"
-DATABASE_HOST="104.154.179.3"
-DATABASE_NAME="thub-prod-sql-db"
-DATABASE_USER="root"
-DATABASE_PASSWORD="THub@200324"
- 
+RAZORPAY_SECRET = "mRcMlDUqNU21VNSiVUi9pxpg";
+RAZORPAY_KEY_ID = "rzp_live_L6Fy6yBDycyCzw";
+EMAIL_SECRET_KEY = "3oT8F4sm02jUIxoT91@ApxvPQ1z!0@";
+GOOGLE_CLIENT_ID =
+  "378678297066-q6qeqtpfh0ih4e99lv887o1rgduehs9u.apps.googleusercontent.com";
+GOOGLE_CLIENT_SECRET = "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";
+GITHUB_CLIENT_ID = "Ov23liLgDH9KQ9QZbAFc";
+GITHUB_CLIENT_SECRET = "68edb40747f174cc7964bf9e24226c46546f9eb6";
+DATABASE_TYPE = "mysql";
+DATABASE_PORT = "3306";
+DATABASE_HOST = "34.42.24.163";
+DATABASE_NAME = "thub-sql-db";
+DATABASE_USER = "root";
+DATABASE_PASSWORD = "THub@200324";
+NODE_ENV = "prod";
+
 const client = new OAuth2Client(GOOGLE_CLIENT_ID);
-const PORT =  8080;
+const PORT = 8080;
+
 // MySQL Connection Pool
 const pool = mysql.createPool({
   host: DATABASE_HOST,
   user: DATABASE_USER,
   password: DATABASE_PASSWORD,
   database: DATABASE_NAME,
-  port: DATABASE_PORT
+  port: DATABASE_PORT,
 });
 
 app.use(express.json());
@@ -41,20 +45,14 @@ app.use(express.json());
 const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
-      'https://thub-test-378678297066.us-central1.run.app',
-      'http://test.thub.tech',
-      'http://34.172.179.132:5001',
-      'http://localhost:5173',
-      'http://localhost:8080',
-      'http://localhost:2000',
-      'https://thub.tech',
-      'https://beta.thub.tech',
-      'https://thub-web-server-2-0-378678297066.us-central1.run.app',
-      'https://thub-web-ser-2-0ls-dot-thub-dev-420204.uc.r.appspot.com',
-      'http://34.172.210.189',
-      'https://thub-web-2-0-0-378678297066.us-central1.run.app',
-      'https://app.thub.tech',
-      'https://34.8.165.90'
+      "https://thub-test-378678297066.us-central1.run.app",
+      "http://test.thub.tech",
+      "http://34.172.179.132:5001",
+      "http://localhost:5173",
+      "http://localhost:8080",
+      "http://localhost:2000",
+      "https://thub.tech",
+      "https://beta.thub.tech",
     ];
 
     const regex = /^https?:\/\/([a-z0-9-]+\.)?thub\.tech$/;
@@ -62,17 +60,17 @@ const corsOptions = {
     if (!origin || allowedOrigins.includes(origin) || regex.test(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-    res.status(200).send({ message: 'Thub-Web-Server-2.0.....' });
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Thub-Web-Server-2.0....." });
 });
 
 app.post("/api/auth/google", async (req, res) => {
@@ -106,11 +104,11 @@ app.post("/api/auth/google", async (req, res) => {
     const connection = await pool.getConnection();
 
     const [rows] = await connection.execute(
-      `SELECT subscription_type FROM users WHERE email = ?`,
+      `SELECT subscription_type FROM test_users WHERE email = ?`,
       [email]
     );
 
-    let subscription_type = "free"; 
+    let subscription_type = "free";
 
     if (rows.length > 0) {
       subscription_type = rows[0].subscription_type || "free";
@@ -118,7 +116,7 @@ app.post("/api/auth/google", async (req, res) => {
 
     if (rows.length === 0) {
       const insertUserQuery = `
-        INSERT INTO users (uid, email, access_token, login_type, name, picture, subscription_type)
+        INSERT INTO test_users (uid, email, access_token, login_type, name, picture, subscription_type)
         VALUES (?, ?, ?, ?, ?, ?, ?)
       `;
       await connection.execute(insertUserQuery, [
@@ -132,7 +130,7 @@ app.post("/api/auth/google", async (req, res) => {
       ]);
     } else {
       const updateUserQuery = `
-        UPDATE users 
+        UPDATE test_users 
         SET access_token = ?, login_type = ?, name = ?, picture = ?, subscription_type = ?
         WHERE email = ?
       `;
@@ -148,13 +146,15 @@ app.post("/api/auth/google", async (req, res) => {
 
     connection.release();
 
-    res.json({ id_token, access_token, user: payload ,userId:userId,});
+    res.json({ id_token, access_token, user: payload, userId: userId });
   } catch (error) {
-    console.error("Error exchanging code:", error.response?.data || error.message);
+    console.error(
+      "Error exchanging code:",
+      error.response?.data || error.message
+    );
     res.status(500).json({ error: "Failed to exchange code" });
   }
 });
-
 
 // github
 app.get("/getAccessToken", async (req, res) => {
@@ -201,7 +201,7 @@ app.get("/getuserData", async (req, res) => {
 
     try {
       const [rows] = await connection.execute(
-        "SELECT COUNT(*) as count FROM users WHERE email = ?",
+        "SELECT COUNT(*) as count FROM test_users WHERE email = ?",
         [login]
       );
 
@@ -209,7 +209,7 @@ app.get("/getuserData", async (req, res) => {
       if (rows[0].count === 0) {
         const subscription_type = "free";
         const query = `
-          INSERT INTO users 
+          INSERT INTO test_users 
           (uid, email, access_token, name, login_type, picture, subscription_type, workspace) 
           VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         `;
@@ -222,7 +222,7 @@ app.get("/getuserData", async (req, res) => {
           "github",
           avatar_url,
           subscription_type,
-          workspace || null
+          workspace || null,
         ]);
 
         userData = {
@@ -241,7 +241,7 @@ app.get("/getuserData", async (req, res) => {
         console.log("User already exists");
 
         const [existingUser] = await connection.execute(
-          "SELECT * FROM users WHERE email = ?",
+          "SELECT * FROM test_users WHERE email = ?",
           [login]
         );
         userData = existingUser[0];
@@ -257,63 +257,93 @@ app.get("/getuserData", async (req, res) => {
   }
 });
 
-
 // email register
 
 function generateRandomID() {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+    (
+      +c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
+    ).toString(16)
   );
 }
 
 app.post("/user", async (req, res) => {
   try {
-    console.log("host: ",DATABASE_HOST)
-    console.log("user: ",DATABASE_USER)
-    console.log("password: ",DATABASE_PASSWORD)
-    console.log("database type: ",DATABASE_TYPE)
+    console.log("host: ", DATABASE_HOST);
+    console.log("user: ", DATABASE_USER);
+    console.log("password: ", DATABASE_PASSWORD);
+    console.log("database type: ", DATABASE_TYPE);
     console.log(req.body);
-    const { email, firstName, lastName, phone, password, login_type, subscription_type, subscription_duration, subscription_date,workspace } = await req.body;
+    const {
+      email,
+      firstName,
+      lastName,
+      phone,
+      password,
+      login_type,
+      subscription_type,
+      subscription_duration,
+      subscription_date,
+      workspace,
+    } = await req.body;
     const uid = generateRandomID();
-    const name = firstName+" "+lastName;
-    const saltRounds = 10; 
+    const name = firstName + " " + lastName;
+    const saltRounds = 10;
     password_hash = await bcrypt.hash(password, saltRounds);
-    console.log("inside server::user : ", email, firstName, lastName, phone, password_hash, login_type, subscription_type, subscription_duration, subscription_date,workspace);
+    console.log(
+      "inside server::user : ",
+      email,
+      firstName,
+      lastName,
+      phone,
+      password_hash,
+      login_type,
+      subscription_type,
+      subscription_duration,
+      subscription_date,
+      workspace
+    );
 
     const connection = await pool.getConnection();
 
-      const insertUserQuery = `INSERT INTO users (uid, email, phone, login_type, name, password_hash, subscription_type, subscription_duration, subscription_date,workspace) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?)`;
-      await connection.execute(insertUserQuery, [
-        uid || null,
-        email || null,
-        phone || null,
-        login_type || null,
-        name || null,
-        password_hash || null,
-        subscription_type || null,
-        subscription_duration || null,
-        subscription_date || null,
-        null,
-      ]);
-      res.status(200).json({ message: "user successfully added", userId: uid, workspace: null});
-    
+    const insertUserQuery = `INSERT INTO test_users (uid, email, phone, login_type, name, password_hash, subscription_type, subscription_duration, subscription_date,workspace) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?)`;
+    await connection.execute(insertUserQuery, [
+      uid || null,
+      email || null,
+      phone || null,
+      login_type || null,
+      name || null,
+      password_hash || null,
+      subscription_type || null,
+      subscription_duration || null,
+      subscription_date || null,
+      null,
+    ]);
+    res
+      .status(200)
+      .json({
+        message: "user successfully added",
+        userId: uid,
+        workspace: null,
+      });
+
     connection.release();
   } catch (error) {
     console.error("Error :", error);
 
     res.status(401).send(error);
   }
-})
+});
 
 app.post("/userdata", async (req, res) => {
   const { userId } = req.body;
-  console.log(req.body,"****");
-  
+  console.log(req.body, "****");
 
   try {
     const connection = await pool.getConnection();
 
-    const fetchUser = `SELECT * FROM users WHERE uid = ?`;
+    const fetchUser = `SELECT * FROM test_users WHERE uid = ?`;
     const user = await connection.execute(fetchUser, [userId]);
     console.log("sent user data: ", user[0]);
     connection.release();
@@ -327,69 +357,167 @@ app.post("/userdata", async (req, res) => {
 });
 
 // login register
-  app.post('/loginUser', async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      const connection = await pool.getConnection();
-      
-      // Query to get user data including workspace
-      const [rows] = await connection.execute(
-        `SELECT uid, email, password_hash, workspace 
-         FROM users 
-         WHERE email = ?`,
-        [email]
-      );
-      connection.release();
-  
-      if (rows.length === 0) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      const { uid, password_hash, workspace } = rows[0];
-      const isPasswordMatch = await bcrypt.compare(password, password_hash);
-  
-      if (!isPasswordMatch) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      const token = jwt.sign({ uid, email }, EMAIL_SECRET_KEY);
-  
-      res.status(200).json({
-        message: 'Login successful',
-        token,
-        userId: uid,
-        workspace: workspace || 'beta', 
-      });
-  
-    } catch (error) {
-      console.error('Login error:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+app.post("/loginUser", async (req, res) => {
+  const { email, password } = req.body;
 
-  app.post("/updateUser", async (req, res) => {
-    const { uid, department, role, designation, company, workspace } = req.body;
-    try {
-      const connection = await pool.getConnection();
-      const updateUserQuery = `UPDATE users SET department = ?, role = ?, designation = ?, company = ?, workspace = ? WHERE uid = ?`;
-      await connection.execute(updateUserQuery, [
-        department,
-        role,
-        designation,
-        company,
-        workspace,
-        uid,
-      ]);
-      connection.release();
-      res.status(200).send({ message: "User data updated successfully" });
-    } catch (error) {
-      console.error("Error updating user data:", error);
-      res
-        .status(500)
-        .json({ message: "Error updating user data", error: error.message });
+  try {
+    const connection = await pool.getConnection();
+
+    // Query to get user data including workspace
+    const [rows] = await connection.execute(
+      `SELECT uid, email, password_hash, workspace 
+         FROM test_users 
+         WHERE email = ?`,
+      [email]
+    );
+    connection.release();
+
+    if (rows.length === 0) {
+      return res.status(401).json({ message: "Invalid email or password" });
     }
-  });
+
+    const { uid, password_hash, workspace } = rows[0];
+    const isPasswordMatch = await bcrypt.compare(password, password_hash);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ uid, email }, EMAIL_SECRET_KEY);
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      userId: uid,
+      workspace: workspace || "beta",
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/updateUser", async (req, res) => {
+  const { uid, department, role, designation, company, workspace } = req.body;
+  try {
+    const connection = await pool.getConnection();
+    const updateUserQuery = `UPDATE test_users SET department = ?, role = ?, designation = ?, company = ?, workspace = ? WHERE uid = ?`;
+    await connection.execute(updateUserQuery, [
+      department,
+      role,
+      designation,
+      company,
+      workspace,
+      uid,
+    ]);
+    connection.release();
+    res.status(200).send({ message: "User data updated successfully" });
+  } catch (error) {
+    console.error("Error updating user data:", error);
+    res
+      .status(500)
+      .json({ message: "Error updating user data", error: error.message });
+  }
+});
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.privateemail.com', 
+  port: 465, 
+  secure: true, 
+  auth: {
+    user: 'no-reply@thub.tech', 
+    pass: 'THub@200324',
+  },
+});
+
+app.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const connection = await pool.getConnection();
+
+    const [user] = await connection.execute(
+      `SELECT uid FROM test_users WHERE email = ?`,
+      [email]
+    );
+
+    if (user.length === 0) {
+      connection.release();
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const userId = user[0].uid;
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    const tokenExpiryDate = new Date(Date.now() + 3600000);
+    const tokenExpiry = tokenExpiryDate
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " ");
+
+    await connection.execute(
+      `UPDATE test_users SET reset_token = ?, token_expiry = ? WHERE uid = ?`,
+      [resetToken, tokenExpiry, userId]
+    );
+
+    connection.release();
+    const apiUrl =
+      NODE_ENV === "development"
+        ? "http://localhost:5173"
+        : "https://thub-web-2-0-0-378678297066.us-central1.run.app";
+    const resetURL = `${apiUrl}/auth/reset-password/${resetToken}?uid=${userId}`;
+
+    await transporter.sendMail({
+      from: 'no-reply@thub.tech', 
+      to: email,
+      subject: "Password Reset Request",
+      text: `Please use the following link to reset your password: ${resetURL}`,
+      html: `<p>Please use the following link to reset your password: <a href="${resetURL}">${resetURL}</a></p>`,
+    });
+
+    res.status(200).json({ message: "Password reset link sent" });
+  } catch (error) {
+    console.error("Error in forgot-password:", error);
+    res.status(500).json({
+      message: "Error sending password reset email",
+      error: error.message,
+    });
+  }
+});
+
+app.post("/reset-password/:token", async (req, res) => {
+  const { uid, newPassword } = req.body;
+  const { token } = req.params;
+
+  try {
+    const connection = await pool.getConnection();
+
+    // Fetch the user based on uid and token
+    const [user] = await connection.execute(
+      `SELECT token_expiry, reset_token FROM test_users WHERE uid = ? AND reset_token = ?`,
+      [uid, token]
+    );
+
+    // Hash the new password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+
+    // Update the user's password and reset the token
+    await connection.execute(
+      `UPDATE test_users SET password_hash = ?, reset_token = NULL, token_expiry = NULL WHERE uid = ?`,
+      [hashedPassword, uid]
+    );
+
+    connection.release();
+
+    res.status(200).json({ message: "Password reset successfully" });
+  } catch (error) {
+    console.error("Error resetting password:", error);
+    res
+      .status(500)
+      .json({ message: "Error resetting password", error: error.message });
+  }
+});
+
 
   
 //razorpay Code
