bdaca2967f646063a83f8df6868520d0b5d61482
asamuel-digi <83590864+asamuel-digi@users.noreply.github.com>
2024-11-12 15:36:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 
 const client = new OAuth2Client(prcoess.env.GOOGLE_CLIENT_ID);
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 // MySQL Connection Pool
 const pool = mysql.createPool({
