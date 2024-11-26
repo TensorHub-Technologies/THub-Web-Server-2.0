342633a060c158adca2f0246030ad35346174c77
asamuel-digi <83590864+asamuel-digi@users.noreply.github.com>
2024-11-26 16:06:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ const corsOptions = {
       "http://localhost:2000",
       "https://thub.tech",
       "https://beta.thub.tech",
+      "http://35.193.70.249"
     ];
 
     const regex = /^https?:\/\/([a-z0-9-]+\.)?thub\.tech$/;
