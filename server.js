bb1ae936ade7c958541b6a705b64edde24e2fe6a
asamuel-digi <83590864+asamuel-digi@users.noreply.github.com>
2025-04-23 15:50:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ const corsOptions = {
       "http://35.224.113.191",
       "http://34.31.158.201",
       "https://textiletradebuddy-app-378678297066.us-central1.run.app/",
-      "https://thub-app.wittysand-a4a5c89d.westus2.azurecontainerapps.io/"
+      "https://thub-app.wittysand-a4a5c89d.westus2.azurecontainerapps.io"
     ];
 
     const regex = /^https?:\/\/([a-z0-9-]+\.)?thub\.tech$/;
